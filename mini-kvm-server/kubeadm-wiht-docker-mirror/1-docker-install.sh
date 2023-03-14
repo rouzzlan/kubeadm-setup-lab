a29eb856c313eb0fc3827078a24fe388de3a3595
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2023-03-14 21:20:27 +0100

    scripts tested on other instance. seems to be working

--- mini-kvm-server/kubeadm-wiht-docker-mirror/1-docker-install.sh
+++ mini-kvm-server/kubeadm-wiht-docker-mirror/1-docker-install.sh
@@ -13,28 +13,6 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update
 sudo apt install -y containerd.io docker-ce docker-ce-cli
 
-# Create required directories
-sudo mkdir -p /etc/systemd/system/docker.service.d
-
-# Create daemon json config file
-sudo tee /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2",
-  "registry-mirrors": [ 
-	    "http://mirror.local:5000"
-    ],
-    "insecure-registries" : [
-	    "http://mirror.local:5000",
-	    "https://local-harbour-repo.net"
-    ]
-}
-EOF
-
 # Start and enable Services
 sudo systemctl daemon-reload
 sudo systemctl restart docker
