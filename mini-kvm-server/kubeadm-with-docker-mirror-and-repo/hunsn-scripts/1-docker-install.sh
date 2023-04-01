b302076d5cb20d2725331807aa64cd0ad04926d2
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2023-04-02 00:04:16 +0200

    script for hunsn server.

---
+++ mini-kvm-server/kubeadm-with-docker-mirror-and-repo/hunsn-scripts/1-docker-install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if ! [ $USER = root ]; then
+  echo run this script with sudo
+  exit 3
+fi
+
+# Add repo and Install packages
+sudo apt update
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y containerd.io docker-ce docker-ce-cli
+
+sudo tee /etc/docker/daemon.json <<EOF
+{
+  "registry-mirrors": [
+            "http://mirrors.739.net:5000",
+            "http://mirrors.739.net:5002"
+    ],
+    "insecure-registries" : [
+            "http://mirrors.739.net:5000",
+            "http://mirrors.739.net:5002",
+            "https://harbour.739.net"
+    ]
+}
+EOF
+
+
+# Start and enable Services
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo systemctl enable docker
